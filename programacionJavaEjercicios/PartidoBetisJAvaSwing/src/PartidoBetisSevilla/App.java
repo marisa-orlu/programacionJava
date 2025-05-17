9472ee3faabc983b1bc36a15db43c814853041f0
Marisa <marisaorlu@gmail.com>
2025-05-17 20:36:55 +0200

    Subida de ejercicios

---
+++ programacionJavaEjercicios/PartidoBetisJAvaSwing/src/PartidoBetisSevilla/App.java
@@ -0,0 +1,8 @@
+package PartidoBetisSevilla;
+
+public class App {
+
+	public static void main(String[] args) {
+		VentanaPartido ventana = new VentanaPartido();
+	}
+}
---
+++ programacionJavaEjercicios/PartidoBetisJAvaSwing/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PartidoBetisJAvaSwing {
+	requires java.desktop;
+}
